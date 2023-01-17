81c22cf5e770cd51344e0520e3f79c3f82d6df10
Yegor Kochetkov <kochetkovyegornikolaevich@gmail.com>
2023-01-17 10:08:23 +0200

    feat: add Paginator

---
+++ src/components/Paginator.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link, useSearchParams } from 'react-router-dom';
+import Pagination from '@mui/material/Pagination';
+import PaginationItem from '@mui/material/PaginationItem';
+import { SxProps } from '@mui/material';
+
+interface PaginatorProps {
+	items: number;
+	itemsPerPage: number;
+	currPage: number;
+}
+
+const paginationStyles: SxProps = {
+	margin: '0 auto',
+	maxWidth: 'fit-content',
+};
+
+function Paginator({ items, itemsPerPage, currPage }: PaginatorProps) {
+	if (!items) return null;
+
+	const pagesCount = Math.ceil(items / itemsPerPage);
+	const [searchParams] = useSearchParams();
+	const filterFromParams =
+		searchParams.get('filter') !== null ? searchParams.get('filter') : '';
+
+	return (
+		<Pagination
+			sx={paginationStyles}
+			page={currPage}
+			count={pagesCount}
+			variant='outlined'
+			shape='rounded'
+			renderItem={(item) => (
+				<PaginationItem
+					component={Link}
+					to={`/articles?page=${item.page}&filter=${filterFromParams}`}
+					{...item}
+				/>
+			)}
+		/>
+	);
+}
+
+export default Paginator;
